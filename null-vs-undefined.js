2acfba2c10dc2c7db55e9d0e6515c0a7cc7b9a72
Umme Salma Ali <ummes3527@gmail.com>
2021-02-01 23:58:52 +0600

    null-vs-undefined-practised-for-gathering-more-and-more-knowledge

---
+++ null-vs-undefined.js
@@ -0,0 +1,15 @@
+// let salama;
+// function add(num1,num2){
+//     // console.log(num1+num2);
+//     return result
+// }
+// function add(num1,num2=50){
+//     console.log(num1,num2)
+// }
+
+// const salma =  {name:"umme", phone:3774, address:"ctg"}
+// console.log(salma.phone);
+const fun = undefined;
+// console.log(fun);
+let ages = [23,14,17];
+console.log(ages[11]);
